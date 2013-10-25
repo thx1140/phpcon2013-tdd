0a649848c9b42008a291329cf0a5ffc2d3b2d42d
Krzysztof Szlapinski <krzysztof.szlapinski@allegro.pl>
2013-10-25 19:29:18 +0200

    shouldAuthenticateUserWithCorrectCredentials

--- Src/Phpcon2013/Authentication/Authenticator.php
+++ Src/Phpcon2013/Authentication/Authenticator.php
@@ -2,13 +2,31 @@
 
 namespace Phpcon2013\Authentication;
 
+use Phpcon2013\Repository\UserRepository;
+use Phpcon2013\Security\Hasher;
 
 class Authenticator {
+    /** @var  Hasher */
+    private $hasher;
+
+    /** @var UserRepository  */
+    private $userRepository;
+
+    public function __construct(Hasher $hasher, UserRepository $userRepository)
+    {
+        $this->hasher = $hasher;
+        $this->userRepository = $userRepository;
+    }
 
     public function authenticate($login, $password)
     {
         if (empty($login)) {
             return false;
+        }
+        $hashedPassword = $this->hasher->hash($password);
+        $user = $this->userRepository->findByLoginAndPasswordHash($login,$password);
+        if($user !== null) {
+            return true;
         }
     }
 }
--- Test/Phpcon2013/Authentication/AuthenticatorTest.php
+++ Test/Phpcon2013/Authentication/AuthenticatorTest.php
@@ -1,14 +1,44 @@
 <?php
 
 use Phpcon2013\Authentication\Authenticator;
+use Phpcon2013\Security\Hasher;
+use Phpcon2013\Repository\UserRepository;
+use Phpcon2013\Entity\User;
 
 class AuthenticatorTest extends PHPUnit_Framework_TestCase {
     /**
      * @test
      */
     public function shouldNotAuthenticateUserWithEmptyLogin() {
-        $authenticator = new Authenticator();
+        $authenticator = new Authenticator(new Hasher(''), new UserRepository(array()));
         $result = $authenticator->authenticate('','password');
         $this->assertFalse($result);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldAuthenticateUserWithCorrectCredentials() {
+
+        $hasherMockBuilder = $this->getMockBuilder('Phpcon2013\Security\Hasher');
+        $hasherMockBuilder->disableOriginalConstructor();
+        $hasherMockBuilder->setMethods(array('hash'));
+        $hasherMock = $hasherMockBuilder->getMock();
+        $hasherMock->expects($this->atLeastOnce())
+            ->method('hash')
+            ->with('correctPassword')
+            ->will($this->returnValue('hashedPassword'));
+
+        $userRepositoryMockBuilder = $this->getMockBuilder('Phpcon2013\Repository\UserRepository');
+        $userRepositoryMockBuilder->disableOriginalConstructor();
+        $userRepositoryMock = $userRepositoryMockBuilder->getMock();
+        $userRepositoryMock->expects($this->atLeastOnce())
+            ->method('findByLoginAndPasswordHash')
+            ->with('login', 'correctPassword')
+            ->will($this->returnValue(new User(1,'login','hashedPassword')));
+
+        $authenticator = new Authenticator($hasherMock, $userRepositoryMock);
+        $result = $authenticator->authenticate('login', 'correctPassword');
+        $this->assertTrue($result);
     }
 }
